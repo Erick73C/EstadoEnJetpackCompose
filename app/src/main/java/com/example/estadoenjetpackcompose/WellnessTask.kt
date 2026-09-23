1f17efcf6b145106770612ab3a20a9aa885a11cb
Erick Omar Perez Gonzlaez <s22120250@alumnos.itsur.edu.mx>
2026-09-23 11:57:07 -0600

    Comit 8: Agregamos una lista que genera algunos datos falsos

---
+++ app/src/main/java/com/example/estadoenjetpackcompose/WellnessTask.kt
@@ -0,0 +1,4 @@
+package com.example.estadoenjetpackcompose
+
+data class WellnessTask(val id: Int, val label: String)
+
--- app/src/main/java/com/example/estadoenjetpackcompose/WellnessTaskItem.kt
+++ app/src/main/java/com/example/estadoenjetpackcompose/WellnessTaskItem.kt
@@ -11,10 +11,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Checkbox
 
 @Composable
 fun WellnessTaskItem(
     taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
     onClose: () -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -22,11 +25,19 @@ fun WellnessTaskItem(
         modifier = modifier, verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
-            modifier = Modifier.weight(1f).padding(start = 16.dp),
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp),
             text = taskName
+        )
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
         )
         IconButton(onClick = onClose) {
             Icon(Icons.Filled.Close, contentDescription = "Close")
         }
     }
 }
+
+fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
